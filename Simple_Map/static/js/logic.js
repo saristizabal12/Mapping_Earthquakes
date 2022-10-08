0759435f850ca53004f820de147748cdaf705ced
Sebastian Aristizabal <sebastianaristizabal@Sebastians-MacBook-Pro-2.local>
2022-10-07 21:52:28 -0400

    playing with map dots

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -6,8 +6,8 @@ let map = L.map('mapid').setView([40.7,-94.5], 4);
 
 // change the marker on our map to a point or dot
 L.circle([34.0522, -118.2437], {
-    color: "black",
-    fillColor: '#ffffa1',
+    color: "orange",
+    fillColor: '#f03',
     // fillPpacity: 0.5,
     radius: 300
  }).addTo(map);
@@ -34,6 +34,55 @@ L.circle([34.0522, -118.2437], {
 
 //  Add a marker to the map for Los Angeles, California.
 // let marker = L.marker([34.0522, -118.2437]).addTo(map);
+let cities = [{
+    location: [40.7128, -74.0059],
+    city: "New York City",
+    state: "NY",
+    population: 8398748
+  },
+  {
+    location: [41.8781, -87.6298],
+    city: "Chicago",
+    state: "IL",
+    population: 2705994
+  },
+  {
+    location: [29.7604, -95.3698],
+    city: "Houston",
+    state: "TX",
+    population: 2325502
+  },
+  {
+    location: [34.0522, -118.2437],
+    city: "Los Angeles",
+    state: "CA",
+    population: 3990456
+  },
+  {
+    location: [33.4484, -112.0740],
+    city: "Phoenix",
+    state: "AZ",
+    population: 1660272
+  }
+  ];
+
+// Get data from cities.js
+let cityData = cities;
+
+// Iterate through the cities array
+// for (let i = 0; i < cities; i++)
+
+
+
+// Loop through the cities array and create one marker for each city.
+cityData.forEach(function(city) {
+    console.log(city)
+    L.circleMarker(city.location, {
+        radius: city.population/100000
+    })
+    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+    .addTo(map);
+});
 
 
 // We create the tile layer that will be the background of our map.
